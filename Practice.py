abbe48bc059b5767914c5f3db9dcdbfb3b542cb0
claire-chua <clair3.chua@gmail.com>
2023-05-07 22:57:11 +1000

    added condition to end loop once enter key is pressed

--- Practice.py
+++ Practice.py
@@ -14,15 +14,27 @@ def practice_test():
     sentence_cursor = 0
     error_character = ""
     updated_sentence = ""
-    end_test = time.time() + 60
-    while time.time() < end_test:
+    elapsed_time = 0
+    start_time = 0
+    timer_started = False
+    user_exited = False
+
+    while (sentence_cursor < len(sentence)) and (user_exited is not True):
+        print("\n", elapsed_time)
         k = readkey()
-        if (k == key.BACKSPACE):
+        if not timer_started:
+            start_time = time.time()
+            timer_started = True
+
+        current_time = time.time()
+        elapsed_time = current_time - start_time
+        if k == key.BACKSPACE:
             sentence_cursor -= 1
             updated_sentence = updated_sentence.rstrip(updated_sentence[-1])
-
-
-        elif (k != sentence[sentence_cursor]):
+        elif k == key.ENTER:
+            print("enter key")
+            user_exited = True
+        elif k != sentence[sentence_cursor]:
             error = error + 1
             # print("error")
             sentence_cursor += 1
@@ -31,24 +43,23 @@ def practice_test():
             updated_sentence = (updated_sentence[:sentence_cursor - 1] + error_character)
             # print(updated_sentence[:sentence_cursor] + '|', end='\r')
             # print('test3', end='\r')
-
-        elif (k == sentence[sentence_cursor]):
+        elif k == sentence[sentence_cursor]:
             sentence_cursor += 1
             updated_sentence = updated_sentence + k
             # print(updated_sentence[:sentence_cursor] + '|', end='\r')
             # print('test5', end='\r')
 
-    #get time module
-    #create accuracy + wpm
-    #save input for file handling
 
+        print(k)
 
-
+        # get time module
+        # create accuracy + wpm
+        # save input for file handling
 
         print_user_input_on_one_line(updated_sentence, sentence, sentence_cursor)
         # compare_strings(updated_sentence, sentence)
 
-    print("\n",elapsed_time)
+    print(elapsed_time, end ="\r")
 
 
 def print_user_input_on_one_line(current_input, expected_input, cursor):
