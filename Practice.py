5d05b0189b3df59c63365357bd0796dfcc177111
claire-chua <clair3.chua@gmail.com>
2023-05-07 13:58:20 +1000

    update print functionality to print to one line only, fixed issue with backspacing and incorrect characters

--- Practice.py
+++ Practice.py
@@ -5,44 +5,50 @@ from colour import Color
 generate = RandomSentence()
 sentence = generate.sentence().lower()
 
-#standard_colour = Color("white")
-#error_colour = Color("red")
+
+# standard_colour = Color("white")
+# error_colour = Color("red")
+
 
 def practice_test():
     print(sentence)
     error = 0
     user_input = print("Please type the sentence shown above. \n")
     sentence_cursor = 0
-    error_character =""
+    error_character = ""
+    updated_sentence = ""
+
     while True:
         k = readkey()
-
         if k == key.BACKSPACE:
             sentence_cursor -= 1
-            print(sentence[:sentence_cursor] + '|' + sentence[sentence_cursor:])
+            updated_sentence = updated_sentence.rstrip(updated_sentence[-1])
+            # print('test2', end='\r')
+
         elif k != sentence[sentence_cursor]:
             error = error + 1
-            print("error")
+            # print("error")
             sentence_cursor += 1
             error_character = "" + k
-            print("I've printed the error")
-            updated_sentence = (sentence[:sentence_cursor - 1] + error_character + sentence[sentence_cursor:])
-            print(updated_sentence[:sentence_cursor] + '|' + updated_sentence[sentence_cursor:])
+            # print("I've printed the error")
+            updated_sentence = (updated_sentence[:sentence_cursor - 1] + error_character)
+            # print(updated_sentence[:sentence_cursor] + '|', end='\r')
+            # print('test3', end='\r')
+
         elif k == sentence[sentence_cursor]:
-
-
             sentence_cursor += 1
-            print(sentence[:sentence_cursor] + '|' + sentence[sentence_cursor:])
+            updated_sentence = updated_sentence + k
+            # print(updated_sentence[:sentence_cursor] + '|', end='\r')
+            # print('test5', end='\r')
+
+        print_user_input_on_one_line(updated_sentence, sentence_cursor)
 
 
-    #if error, replace letter with error character, add colour
-    #if correct, change colour
-    #print to the same line
-    #fix git repository
-        
+def print_user_input_on_one_line(current_input, cursor):
+    print('\x1b[2K\r', end='\r')
+    print(current_input[:cursor] + '|', end='\r')
 
-    
-
-
-
-
+    # if error, replace letter with error character, add colour
+    # if correct, change colour
+    # print to the same line
+    # fix git repository
