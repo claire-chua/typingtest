e48445b771c5c9fe6eb931775afb33df8da37b0f
Claire Chua <clair3.chua@gmail.com>
2023-05-07 11:35:44 +1000

    replace sentences with error character and move cursor

--- programming/Assignment 3/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo 'do we see this stuff?'
-python3 -m venv typingtest-venv
-./Scripts/activate
-pip3 install -r Scripts/requirements.txt
-python3 TypingTest.py
--- programming/Assignment3/Practice.py
+++ programming/Assignment3/Practice.py
@@ -5,62 +5,42 @@ from colour import Color
 generate = RandomSentence()
 sentence = generate.sentence().lower()
 
-# sentence = generate.sentence()
-
 #standard_colour = Color("white")
 #error_colour = Color("red")
-
-# def slice_sentence(string, start, end):
-#     return string[start:end]
 
 def practice_test():
     print(sentence)
     error = 0
     user_input = print("Please type the sentence shown above. \n")
     sentence_cursor = 0
-
-    #print (sentence[:sentence_cursor+1] + '|' + sentence[sentence_cursor+1:])
-
+    error_character =""
     while True:
-        # k = readkey()
-        # if k == "a":
-        #     print("hi")
-        # if k == key.DOWN:
-        #     print("hi")
-        # if k == key.ENTER:
-        #     break
         k = readkey()
 
         if k == key.BACKSPACE:
             sentence_cursor -= 1
-            print("you're trying to backspace")
+            print(sentence[:sentence_cursor] + '|' + sentence[sentence_cursor:])
         elif k != sentence[sentence_cursor]:
             error = error + 1
             print("error")
             sentence_cursor += 1
+            error_character = "" + k
+            print("I've printed the error")
+            updated_sentence = (sentence[:sentence_cursor - 1] + error_character + sentence[sentence_cursor:])
+            print(updated_sentence[:sentence_cursor] + '|' + updated_sentence[sentence_cursor:])
         elif k == sentence[sentence_cursor]:
+
+
             sentence_cursor += 1
+            print(sentence[:sentence_cursor] + '|' + sentence[sentence_cursor:])
 
-        print (sentence[:sentence_cursor] + '|' + sentence[sentence_cursor:])
 
-    
-
+    #if error, replace letter with error character, add colour
+    #if correct, change colour
+    #print to the same line
+    #fix git repository
         
 
-        # print (slice_sentence(sentence, sentence_cursor, len(sentence)))
-
-        # for i in range(sentence_cursor, len(sentence)):
-        #     print sentence[i:i+3]
-
-        
-
-            #character.error_colour
-        # elif keyboard.is_pressed("enter"):
-        #     print('Key pressed')
-        #     break
-        # else:
-        #     none
-        #         #character.standard_colour
     
 
 
