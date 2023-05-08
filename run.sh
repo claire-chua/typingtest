5ad4654cbfc6d7ab6cac493d7c6e362f46f88958
claire-chua <clair3.chua@gmail.com>
2023-05-09 01:36:34 +1000

    added new test, to check if function does as intended

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@ pip install virtualenv
 python -m virtualenv env
 source venv/bin/activate
 pip install -r requirements.txt
+pytest Test/
 python3 TypingTestMenu.py
 
--- tests/PracticeTest.py
+++
@@ -1,2 +0,0 @@
-# spy mocker
-# matched words
