d7c8b3851956a9670195bd73cb6ee18050249677
claire-chua <clair3.chua@gmail.com>
2023-05-08 17:08:45 +1000

    removed pandas package, as not utilised

--- Scoreboard.py
+++ Scoreboard.py
@@ -1,17 +1,19 @@
 from Practice import *
-# import pandas as pd
+
 
 opened_file = False
 # check_file = pd.read_csv("scores.csv")
-# or check_file.empty == True
+
+
 if opened_file == False:
     with open("scores.csv", 'a', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(["WPM, Accuracy(%)"])
-        opened_file == True
+        opened_file = True
+
 
 def scoreboard():
-    with open("scores.csv", 'r', newline = '') as file:
+    with open("scores.csv", 'r', newline='') as file:
         reader = csv.reader(file)
         for row in reader:
             print(row)
